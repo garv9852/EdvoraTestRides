28b6119343fc3655995e6ac5b24bd41c7e52851d
Garv kapoor <Garvk@LAPTOP-DEDK2DPV.(none)>
2022-02-18 18:37:19 +0530

    v2

--- src/components/Login.js
+++ src/components/Login.js
@@ -27,7 +27,7 @@ function Login(props) {
         props.ChangeUser(user);
       }
       reader.readAsDataURL(file);
-      history(`/ride${stationCode}`);
+      history(`EdvoraTestRides/ride${stationCode}`);
     }
   return (
     <div>
